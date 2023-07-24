b9371908080cd27fb2b560001f1613f653f54aa0
nvPhi@02 <nguyenvanphidmx123455@gmail.com>
2023-07-24 23:54:59 +0700

    build production

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 // import reactlogo from './assets/images/react.svg'
 import './assets/styles/app.css'
 import './assets/styles/app.scss'
+import Home from './Home'
 
 const App: FC = () => {
   return (
@@ -9,6 +10,8 @@ const App: FC = () => {
       {/* <img src={reactlogo} alt='React Logo' width={100} height={100} /> */}
       <h1>{'Nguyễn Vạn Phi'}</h1>
       <h2>Bài viết được viết tại blog</h2>
+
+      <Home />
     </div>
   )
 }
